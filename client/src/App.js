1bc854c92c2bd5538c707a1a26e4fd83cea848c7
Andrey-Savitskiy <ansav27@yandex.ru>
2023-06-25 15:27:16 +0300

    [frontend] Add normalize.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import './style/normalize.css';
+import './style/App.css';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
