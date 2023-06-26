4360799a73a70c7147d0b910820187e163974411
Andrey-Savitskiy <ansav27@yandex.ru>
2023-06-26 16:05:08 +0300

    [frontend] Fork CurrencyRate to SELL and BUY lists

--- client/src/components/CurrencyRate.js
+++ client/src/components/CurrencyRate.js
@@ -1,20 +1,37 @@
 import React, {useState} from 'react';
 import '../style/components/CurrencyRate.css';
 import CurrencyRateListItem from "./CurrencyRateListItem";
+import CurrencyRateList from "./CurrencyRateList";
 
 const CurrencyRate = () => {
-    const [content, setCurrencyRate] = useState([
+    const [contentSell, setCurrencyRateSell] = useState([
         {id: 1, name: 'EUR/USD', cost: 1.089900},
         {id: 2, name: 'EUR/RUB', cost: 92.3400},
         {id: 3, name: 'USD/RUB', cost: 84.7000},
     ])
 
+    const [contentBuy, setCurrencyRateBuy] = useState([
+        {id: 1, name: 'EUR/USD', cost: 1.09109},
+        {id: 2, name: 'EUR/RUB', cost: 93.3460},
+        {id: 3, name: 'USD/RUB', cost: 86.5670},
+    ])
+
     return (
         <article className={'currency-rate'}>
             <h3 className={'currency-rate-header'}>Курсы валютных пар:</h3>
-            <ul className={'currency-rate-list'}>
-                {content.map(item => <CurrencyRateListItem content={item} key={item.id}/>)}
-            </ul>
+
+            <div className={'currency-rate-container'}>
+                <CurrencyRateList
+                    idPrefix={'sell'}
+                    content={{header: 'Продажа', content: contentSell}}
+                    style={{color: 'red'}}
+                />
+                <CurrencyRateList
+                    idPrefix={'buy'}
+                    content={{header: 'Купля', content: contentBuy}}
+                    style={{color: 'green'}}
+                />
+            </div>
         </article>
     );
 };
--- client/src/components/CurrencyRateListItem.js
+++ client/src/components/CurrencyRateListItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CurrencyRateListItem = (props) => {
     return (
         <li className={'currency-rate-list-item'}>
-            <p>{props.content.name}: <strong>{props.content.cost}</strong></p>
+            <p id={`${props.content.name}-${props.idPrefix}`}>{props.content.name}: <strong>{props.content.cost}</strong></p>
         </li>
     );
 };
