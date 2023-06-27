c161446c80a10fd7a2cfd319dc63c14666d3d819
MrBlackDog <sasha29091999@yandex.ru>
2023-06-27 11:56:25 +0300

    [initial] Create start server project

--- server/app.py
+++ server/app.py
@@ -1,32 +1,21 @@
-import mimetypes
-import pathlib
-
-import fastapi
-
-import ntpro_server
-
-api = fastapi.FastAPI()
-server = ntpro_server.NTProServer()
-html = pathlib.Path('test.html').read_text()
+from fastapi import FastAPI, WebSocket
 
 
-@api.get('/')
-async def get():
-    return fastapi.responses.HTMLResponse(html)
+app = FastAPI()
 
 
-@api.get('/static/{path}')
-async def get(path: pathlib.Path):
-    static_file = (pathlib.Path('static') / path).read_text()
-    mime_type, encoding = mimetypes.guess_type(path)
-    return fastapi.responses.PlainTextResponse(static_file, media_type=mime_type)
+@app.websocket("/ws/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        try:
+            message = await websocket.receive_json()
+            print(message)
+        except Exception as error:
+            continue
 
 
-@api.websocket('/ws/')
-async def websocket_endpoint(websocket: fastapi.WebSocket):
-    await server.connect(websocket)
+if __name__ == "__main__":
+    import uvicorn
 
-    try:
-        await server.serve(websocket)
-    except fastapi.WebSocketDisconnect:
-        server.disconnect(websocket)
+    uvicorn.run(app, port=8000)
